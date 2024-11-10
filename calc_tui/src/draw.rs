c09e48b38c95f1b946448ea73da1dd6dc0b636de
wilwe <wilwe036@gmail.com>
2024-11-10 08:50:35 +0100

    grindset

--- calc_tui/src/draw.rs
+++ calc_tui/src/draw.rs
@@ -1,25 +1,56 @@
-pub struct box {
-    childerns: Vec<_>,
-    orientation: String,
-}
 pub struct squere {
-    x: u16,
-    y: u16,
+    color: u8,
+    text: String,
+    text_color: u8,
     h: u16,
-    z: u16,
+    w: u16,
 }
-pub fn draw() {
-    let frame_bg = "3";
-    let frame_fg = "0";
-    let width = 50;
-    let height = 30;
-    let title = "calc";
-    println!("\x1b[48;5;{}m\x1b[38;5;{}m\x1b[1A", frame_bg, frame_fg);
-    println!("{:^width$}", title);
-    println!("\x1b[0m\x1b[1A");
-    let w2 = width - 2;
-    for i in 0..(height-2) {
-        println!("\x1b[48;5;{}m \x1b[0m{:^w2$}\x1b[48;5;{}m \x1b[0m",frame_bg, " ", frame_bg);
+impl squere {
+    pub fn new(color: u8, text: String, text_color: u8, h:u16, w:u16) -> Self {
+        Self {
+            color,
+            text,
+            text_color,
+            h,
+            w,
+        }
     }
-    println!("\x1b[48;5;{}m{:^width$}\x1b[0m",frame_bg,"");
+    pub fn draw(self) {
+        print!("\x1b[48;5;{}m\x1b[38;5;{}m{}\x1b[0m",self.color,self.text_color,self.text)
+    }
+}
+pub struct frame {
+    width: u16,
+    height: u16,
+    color: u8,
+    title: String,
+    title_color: u8,
+}
+impl frame {
+    pub fn new(width: u16, height: u16, color: u8, title: String, title_color: u8) -> Self {
+        Self {
+            width,
+            height,
+            color,
+            title,
+            title_color,
+        }
+    }
+    pub fn draw(&self) {
+        let w: usize = (self.width).into();
+        println!("\x1b[48;5;{}m\x1b[38;5;{}m\x1b[1A", self.color, self.title_color);
+        println!("{:^w$}", self.title);
+        println!("\x1b[0m\x1b[1A");
+        let w2: usize = (self.width - 2).into();
+        for i in 0..(self.height-2) {
+            println!("\x1b[48;5;{}m \x1b[0m{:^w2$}\x1b[48;5;{}m \x1b[0m",self.color, " ", self.color);
+        }
+        print!("\x1b[48;5;{}m{:^w$}\x1b[0m\x1b[{}A\x1b[{}D",self.color,"",(self.height-2),(self.width-1));
+    }
+    pub fn goto_line(&self,line:u16) {
+        print!("\x1b[{};2H", (line+2));
+    }
+    pub fn end(&self) {
+        print!("\x1b[{};0H", (self.height+2));
+    }
 }
--- calc_tui/src/main.rs
+++ calc_tui/src/main.rs
@@ -1,7 +1,15 @@
 mod draw;
 
-use crate::draw::draw;
+use crate::draw::*;
 
 fn main() {
-    draw();
+    println!("\x1b[2J\x1b[0;0H");
+    let w = draw::frame::new(50,30,3,"calc".to_string(),0);
+    let but = draw::squere::new(4,"chuj".to_string(), 0, 0,0);
+    let butt = draw::squere::new(4,"chuj".to_string(), 0, 0,0);
+    w.draw();
+    but.draw();
+    w.goto_line(2);
+    butt.draw();
+    w.end();
 }
