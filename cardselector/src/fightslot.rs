ba9ea3aeed5657838fdd48b5a44b264bf5676058
wilwe <wilwe036@gmail.com>
2025-05-21 07:04:07 +0200

    slot s

--- cardselector/src/fightslot.rs
+++ cardselector/src/fightslot.rs
@@ -1,4 +1,19 @@
+use crate::FightCard::FightCard;
+
+use core::fmt;
+
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct FightSlot {
+    id: u8,
+    card: Option<FightCard>
+}
 
+impl fmt::Display for FightSlot {
+    fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Ressult {
+        if self.card.is_some() {
+            write!(f, "{}", Some(self.card))
+        } else {
+            write!(f, "sus")
+        }
+    }
 }
